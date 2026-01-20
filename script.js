84d188dbc6cc700f5d7ca073b23e70bc51f89fdc
tenthweb <47951574+tenthweb@users.noreply.github.com>
2026-01-20 23:51:17 +0000

    Refactor drawing layers and update canvas size

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+let layerA, layerB;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  layerA = createGraphics(width, height);
+  layerB = createGraphics(width, height);
+
+  makeLayer(layerA, 200, 200, 150);
+  makeLayer(layerB, 300, 200, 150);
+}
+
+function makeLayer(g, x, y, r) {
+  g.clear();
+  g.background(0);
+  g.erase();
+  g.circle(x, y, r * 2);
+  g.noErase();
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 50);
+  background(255);
+  blendMode(MULTIPLY);
+  image(layerA, 0, 0);
+  image(layerB, 0, 0);
+  blendMode(BLEND);
 }
